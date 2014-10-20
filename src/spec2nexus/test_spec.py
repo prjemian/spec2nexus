5223de06d7171fba11b127f41facfe8f0016105a
Pete Jemian <prjemian@gmail.com>
2014-10-20 17:53:09 -0500

    add copyright

--- src/spec2nexus/test_spec.py
+++ src/spec2nexus/test_spec.py
@@ -2,6 +2,16 @@
 unit tests for the spec module
 '''
 
+#-----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2014, Pete R. Jemian
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
 import unittest
 from spec2nexus import spec, utils
 import os
