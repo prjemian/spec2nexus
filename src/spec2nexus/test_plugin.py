76a0ad7866dbb4f18b8bf26992ad8b24754d27c1
Pete Jemian <prjemian@gmail.com>
2014-10-20 15:17:37 -0500

    more tests

--- src/spec2nexus/test_plugin.py
+++ src/spec2nexus/test_plugin.py
@@ -3,12 +3,17 @@ unit tests for the plugin module
 '''
 import unittest
 import plugin
+import os
 
 
 class Test(unittest.TestCase):
 
-#     def setUp(self):
-#         pass
+    def setUp(self):
+        os.environ['SPEC2NEXUS_PLUGIN_PATH'] = 'C://Users//Pete//Desktop, /tmp'
+        self.basepath = os.path.abspath(os.path.dirname(__file__))
+        self.datapath = os.path.join(self.basepath, 'data')
+        self.manager = plugin.ControlLineHandlerManager()
+        self.manager.load_plugins()
 
 #     def tearDown(self):
 #         pass
@@ -20,37 +25,27 @@ class Test(unittest.TestCase):
         h = plugin.ControlLineHandler()
         self.assertEqual(h.key, None)
         self.assertEqual(str(h.key), h.getKey())
-
-# TODO: suggestions for testing the plugin architecture
-# def simple_test():
-#     os.environ[PLUGIN_SEARCH_PATH_ENVIRONMENT_VARIABLE] = 'C://Users//Pete//Desktop, /tmp'
-#     manager = ControlLineHandlerManager()
-#     manager.load_plugins()
-#     pprint.pprint(manager.handler_dict)
-# 
-#     spec_data = '''
-#         #S 1 ascan eta 43.6355 44.0355 40 1
-#         #D Thu Jul 17 02:38:24 2003
-#         #T 1 (seconds)
-#         #G0 0 0 0 0 0 1 0 0 0 0 0 0 50 0 0 0 1 0 0 0 0
-#         #V110 101.701 56 1 4 1 1 1 1 992.253
-#         #@CHANN 1201 1110 1200 1
-#         #N 14
-#         #L eta H K L elastic Kalpha Epoch seconds signal I00 harmonic signal2 I0 I0
-#         #@MCA 16C
-#         #@CHANN 1201 1110 1200 1
-#         #o0 un0 mx my waxsx ax un5 az un7
-#         @A 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\
-#         0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-#         #H4 FB_o2_on FB_o2_r FB_o2_sp
-#         #Pete wrote this stuff
-#         43.6835 0.998671 -0.0100246 11.0078 1 0 66 1 0 863 0 0 1225 1225
-#     '''
-#     for spec_line in spec_data.strip().splitlines():
-#         txt = spec_line.strip()
-#         if len(txt) > 0:
-#             key = manager.getKey(txt)
-#             print key, manager.get(key)
+    
+    def test_sample_control_line_keys(self):
+        spec_data = {
+            '#S'           : r'#S 1 ascan eta 43.6355 44.0355 40 1',
+            '#D'           : r'#D Thu Jul 17 02:38:24 2003',
+            '#T'           : r'#T 1 (seconds)',
+            '#G\\d+'       : r'#G0 0 0 0 0 0 1 0 0 0 0 0 0 50 0 0 0 1 0 0 0 0',
+            '#V\\d+'       : r'#V110 101.701 56 1 4 1 1 1 1 992.253',
+            '#N'           : r'#N 14',
+            '#L'           : r'#L eta H K L elastic Kalpha Epoch seconds signal I00 harmonic signal2 I0 I0',
+            '#@MCA'        : r'#@MCA 16C',
+            '#@CHANN'      : r'#@CHANN 1201 1110 1200 1',
+            '#o\d+'        : r'#o0 un0 mx my waxsx ax un5 az un7',
+            '@A'           : r'@A 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\\',
+            'scan data'    : r' 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'.lstrip(),
+            '#H\\d+'       : r'#H4 FB_o2_on FB_o2_r FB_o2_sp',
+            None           : r'#Pete wrote this stuff',
+            'scan data'    : r'43.6835 0.998671 -0.0100246 11.0078 1 0 66 1 0 863 0 0 1225 1225',
+        }
+        for k, v in spec_data.items():
+            self.assertEqual(k, self.manager.getKey(v))
 
 
 if __name__ == "__main__":
