89cf2789008a8aa130c115013c938665c1db43ca
John Hammond <JPHammonds@anl.gov>
2016-05-17 15:45:09 -0500

    Add in writer to add information to output HDF files.

--- src/spec2nexus/plugins/XPCS_spec2nexus.py
+++ src/spec2nexus/plugins/XPCS_spec2nexus.py
@@ -1,16 +1,28 @@
 from spec2nexus.plugin import ControlLineHandler, strip_first_word
+from spec2nexus.eznx import makeGroup
+from spec2nexus.spec import SpecDataFileHeader
 
 class XPCS_VA(ControlLineHandler):
-    '''**#VA** '''
+    '''**#VA**'''
 
     key = '#VA\d+'
 
     def process(self, text, spec_obj, *args, **kws):
+        subkey = text.split()[0].lstrip('#')
         if not hasattr(spec_obj, 'VA'):
-            spec_obj.VA = []
-            
-        spec_obj.VA.append( strip_first_word(text) )
+            spec_obj.VA = {}
+        spec_obj.VA[subkey] = strip_first_word(text)
+        if isinstance(spec_obj, SpecDataFileHeader):
+            spec_obj.addH5writer(self.key, self.writer)
     
+    def writer(self, h5parent, writer, scan, nxclass=None, *args, **kws):
+        ''' Describe how to write VA data'''
+        desc = "XPCS VA parameters"
+        group = makeGroup(h5parent, 'VA', nxclass,descriuption=desc)
+        dd = {}
+        for item, value in scan.VA.items():
+            dd[item] = map(str, value.split())
+        writer.save_dict(group, dd)
 
 class XPCS_VD(ControlLineHandler):
     '''**#VD** '''
@@ -18,12 +30,22 @@ class XPCS_VD(ControlLineHandler):
     key = '#VD\d+'
 
     def process(self, text, spec_obj, *args, **kws):
+        subkey = text.split()[0].lstrip('#')
         if not hasattr(spec_obj, 'VD'):
-            spec_obj.VD = []
+            spec_obj.VD = {}
             
-        spec_obj.VD.append( strip_first_word(text) )
-        #spec_obj.addPostProcessor('unicat_metadata', self.postprocess)
+        spec_obj.VD[subkey] = strip_first_word(text)
+        if isinstance(spec_obj, SpecDataFileHeader):
+            spec_obj.addH5writer(self.key, self.writer)
     
+    def writer(self, h5parent, writer, scan, nxclass=None, *args, **kws):
+        ''' Describe how to write VD data'''
+        desc = "XPCS VD parameters"
+        group = makeGroup(h5parent, 'VD', nxclass, descriuption=desc)
+        dd = {}
+        for item, value in scan.VD.items():
+            dd[item] = map(str, value.split())
+        writer.save_dict(group, dd)
 
 class XPCS_VE(ControlLineHandler):
     '''**#VE** '''
@@ -31,9 +53,19 @@ class XPCS_VE(ControlLineHandler):
     key = '#VE\d+'
 
     def process(self, text, spec_obj, *args, **kws):
+        subkey = text.split()[0].lstrip('#')
         if not hasattr(spec_obj, 'VE'):
-            spec_obj.VE = []
+            spec_obj.VE = {}
             
-        spec_obj.VE.append( strip_first_word(text) )
-        #spec_obj.addPostProcessor('unicat_metadata', self.postprocess)
+        spec_obj.VE[subkey] = strip_first_word(text)
+        if isinstance(spec_obj, SpecDataFileHeader):
+            spec_obj.addH5writer(self.key, self.writer)
     
+    def writer(self, h5parent, writer, scan, nxclass=None, *args, **kws):
+        ''' Describe how to write VE data'''
+        desc = "XPCS VE parameters"
+        group = makeGroup(h5parent, 'VE', nxclass,descriuption=desc)
+        dd = {}
+        for item, value in scan.VE.items():
+            dd[item] = map(str, value.split())
+        writer.save_dict(group, dd)
