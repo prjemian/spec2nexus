91d923789deb075791f57318983adb7b7cf5fd52
jemian <prjemian@gmail.com>
2017-01-30 14:39:23 -0600

    refs #89 prep

---
+++ conda-recipe/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+$PYTHON setup.py install
+
+# NeXpy uses:
+# $PYTHON setup.py install --single-version-externally-managed --record installed_files.txt
+
+# Add more build steps here, if they are necessary.
