06dab05cabae0c97dd712751faf82e3eebeffaba
jemian <prjemian@gmail.com>
2017-01-30 14:42:20 -0600

    refs #59: installation

--- src/spec2nexus/__init__.py
+++ src/spec2nexus/__init__.py
@@ -36,7 +36,7 @@ __url__          = u'http://spec2nexus.readthedocs.org'
 #__download_url__ = u'https://github.com/prjemian/spec2nexus/tarball/' + __version__
 __keywords__     = ['SPEC', 'diffraction', 'data acquisition', 'NeXus', 'HDF5', 'MatPlotLib']
 
-__install_requires__ = ('h5py','numpy', 'MatPlotLib', )
+__install_requires__ = ('h5py','numpy', )
 __classifiers__ = [
      'Development Status :: 5 - Production/Stable',
      'Environment :: Console',
