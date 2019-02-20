ac2748448dda040e5c8783d9f0b5df8d9222269f
Pete Jemian <prjemian@gmail.com>
2019-02-20 13:19:29 -0600

    DOC

--- src/spec2nexus/dev_datafile.py
+++ src/spec2nexus/dev_datafile.py
@@ -16,14 +16,14 @@ if __name__ == '__main__':
     pwd = os.path.abspath(os.getcwd())
     prefix = os.path.abspath(os.path.join(pwd, 'data', 'APS_spec_data'))
     file1 = prefix + '.dat'
-    # lmn40.spe has two header section, 2nd is just before "#S 8"
+    hfile = prefix + '.hdf5'
+    # lmn40.spe has two header sections, 2nd is just before "#S 8"
     prefix = os.path.abspath(os.path.join(pwd, 'data', 'lmn40'))
     file1 = prefix + '.spe'
-    hfile = prefix + '.hdf5'
-    # CdOsO has two header sections and two #S 1 scans
+
+    # CdOsO has four header sections and two #S 1 scans
     prefix = os.path.abspath(os.path.join(pwd, 'data', 'CdOsO'))
     file1 = prefix
-    hfile = prefix + '.hdf5'
 
     # writer interface has changed, must use new spec module to proceed
     specfile = spec.SpecDataFile(file1)
