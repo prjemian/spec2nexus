6213666e112e12ea870db20a9330d16f92546d7a
Pete Jemian <prjemian@gmail.com>
2019-04-25 16:11:57 -0500

    #128 unit test for problem

---
+++ tests/issue128.py
@@ -0,0 +1,71 @@
+
+'''
+test issue 128
+'''
+
+#-----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2014-2019, Pete R. Jemian
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+import os
+import sys
+import unittest
+
+_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src'))
+if _path not in sys.path:
+    sys.path.insert(0, _path)
+
+from spec2nexus import spec
+
+_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if _path not in sys.path:
+    sys.path.insert(0, _path)
+import tests.common
+
+
+class Issue128(unittest.TestCase):
+   
+    def setUp(self):
+        path = os.path.dirname(spec.__file__)
+        self.testfile = os.path.join(path, 'data', 'CdOsO')
+        self.sys_argv0 = sys.argv[0]
+
+    def tearDown(self):
+        sys.argv = [self.sys_argv0,]
+
+    def test_data_file(self):
+        self.assertTrue(os.path.exists(self.testfile))
+
+        specData = spec.SpecDataFile(self.testfile)
+        self.assertTrue(isinstance(specData, spec.SpecDataFile))
+
+        with self.assertRaises(ValueError):
+            # #128 failed due to assumption of int keys
+            r = sorted(specData.scans.keys(), key=int)
+
+        # next line assumes #128 is fixed
+        scans = specData.getScanNumbers()
+        self.assertEqual(len(scans), 74, "expected number of scans")
+        self.assertTrue("1" in scans)
+        self.assertTrue("1.1" in scans)
+
+
+def suite(*args, **kw):
+    test_list = [
+        Issue128,
+        ]
+    test_suite = unittest.TestSuite()
+    for test_case in test_list:
+        test_suite.addTest(unittest.makeSuite(test_case))
+    return test_suite
+
+
+if __name__ == '__main__':
+    runner=unittest.TextTestRunner()
+    runner.run(suite())
