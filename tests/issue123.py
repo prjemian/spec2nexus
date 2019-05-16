48658fc5dffc4b4ea28626cdfea696eaa2337895
Pete Jemian <prjemian@gmail.com>
2019-05-15 23:16:34 -0500

    fixes #145

--- tests/issue123.py
+++ tests/issue123.py
@@ -48,13 +48,40 @@ class Issue123(unittest.TestCase):
 
         specData = spec.SpecDataFile(testfile)
         self.assertTrue(isinstance(specData, spec.SpecDataFile))
+        
+        self.assertEqual(len(specData.headers), 1, "expected number of headers")
+        header = specData.headers[0]
+        self.assertTrue(isinstance(header, spec.SpecDataFileHeader))
+        self.assertEqual(len(header.raw), 0, "no raw content")
+        self.assertEqual(len(header.date), 0, "default date")
+        self.assertEqual(header.epoch, 0, "default epoch")
+        self.assertEqual(len(header.comments), 0, "expected number of header comments")
+        self.assertEqual(len(header.O), 0, "defined positioner label rows")
+        self.assertEqual(len(header.H), 0, "defined metadata label rows")
 
         scans = specData.getScanNumbers()
         self.assertEqual(len(scans), 171, "expected number of scans")
-        
-        self.assertEqual(len(specData.headers), 1, "expected number of headers")
-        self.assertEqual(len(specData.headers[0].raw), 0, "no raw content")
-        # TODO: test more content
+        scan = specData.getScan(2)
+        self.assertTrue(isinstance(scan, spec.SpecDataFileScan))
+
+        self.assertEqual(len(scan.P), 20, "defined positioner value rows")
+        self.assertEqual(len(header.O), 20, "defined positioner label rows")
+        self.assertEqual(scan.header.O, header.O, "same object")
+        self.assertEqual(len(scan.V), 0, "defined metadata value rows")
+        self.assertEqual(len(scan.L), 23, "defined data column labels")
+        self.assertEqual(len(scan.data), 23, "defined data variables")
+        self.assertEqual(scan.L[0], "Pt_No", "first data column label")
+        self.assertEqual(scan.L[-1], "dt", "last data column label")
+        d1 = scan.data.get("Pt_No")
+        self.assertNotEqual(d1, None, "data 'Pt_No' exists")
+        self.assertEqual(len(d1), 51, "data 'Pt_No' has expected number of values")
+        self.assertEqual(scan.scanCmd, "dscan th -0.5 0.5 50 1.0", "scan command")
+        self.assertEqual(scan.scanNum, "2", "scan number as string")
+        self.assertNotEqual(scan.scanNum, 2, "scan number as integer")
+        self.assertEqual(len(scan.positioner), 155, "defined positioners")
+        d1 = scan.positioner.get("abs")
+        self.assertNotEqual(d1, None, "positioner 'abs' exists")
+        self.assertEqual(d1, 0.0, "positioner 'abs' value is zero")
 
     def test_33id_file(self):
         testfile = os.path.join(self.path, 'data', '33id_spec.dat')
@@ -70,8 +97,15 @@ class Issue123(unittest.TestCase):
         scans = specData.getScanNumbers()
         self.assertEqual(len(scans), 106, "expected number of scans")
         
-        # TODO: test more content
         self.assertEqual(len(specData.headers), 1, "expected number of headers")
+        header = specData.headers[0]
+        self.assertTrue(isinstance(header, spec.SpecDataFileHeader))
+        self.assertGreater(len(header.raw), 0, "raw content")
+        self.assertGreater(len(header.date), 0, "defined date")
+        self.assertEqual(header.epoch, 1058427452, "expected epoch")
+        self.assertEqual(len(header.comments), 2, "expected number of header comments")
+        self.assertEqual(len(header.O), 4, "defined positioner label rows")
+        self.assertEqual(len(header.H), 21, "defined metadata label rows")
 
 
 def suite(*args, **kw):
