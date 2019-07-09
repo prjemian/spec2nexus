f91fab62c97828d806992d11d35bfdab9c91fc59
Peter Jemian <jemian@wow.aps.anl.gov>
2019-07-09 18:06:18 -0500

    PKG revise

--- publish.sh
+++ publish.sh
@@ -6,7 +6,10 @@
 
 PACKAGE=spec2nexus
 RELEASE=`python setup.py --version`
-
+if [[ ${RELEASE} == *dirty ]] ; then
+  echo "version: ${RELEASE} not ready to publish"
+  exit 1
+fi
 ## PyPI Build and upload::
 
 python setup.py sdist bdist_wheel
@@ -16,18 +19,26 @@ twine upload dist/${PACKAGE}-${RELEASE}*
 
 ### Conda channels
 
-# `prjemian`    personal channel
-# `aps-anl-tag` production releases
-# `aps-anl-dev` anything else, such as: pre-release, release candidates, or testing purposes
-CHANNEL=prjemian
+# if [[ ${RELEASE} == \'*\' ]]
+if [[ ${RELEASE} == *rc* ]] ; then
+  # anything else, such as: pre-release, release candidates, or testing purposes
+  CHANNEL=aps-anl-dev
+else
+  # production releases
+  CHANNEL=aps-anl-tag
+fi
 
-### publish
+### publish (from linux)
 
-CONDA_BLD_PATH=/tmp/conda-bld
+export CONDA_BLD_PATH=/tmp/conda-bld
 /bin/rm -rf ${CONDA_BLD_PATH}
-/bin/mkdir ${CONDA_BLD_PATH}
+/bin/mkdir -p ${CONDA_BLD_PATH}
 
 conda build ./conda-recipe/
 BUILD_DIR=${CONDA_BLD_PATH}/noarch
 BUNDLE=${BUILD_DIR}/${PACKAGE}-${RELEASE}-py*_0.tar.bz2
+anaconda upload -u ${CHANNEL} ${BUNDLE}
+
+# also post to my personal channel
+CHANNEL=prjemian
 anaconda upload -u ${CHANNEL} ${BUNDLE}
