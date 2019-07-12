0a9b74c8853846d0a0c160fbfd29f96810c487ad
Pete Jemian <prjemian@gmail.com>
2019-07-12 09:24:19 -0500

    DOC #166 acknowledgment

--- tests/trial/plugin_base.py
+++ tests/trial/plugin_base.py
@@ -2,12 +2,14 @@
 """
 test of "Using Metaclasses to Create Self-Registering Plugins"
 
-see: https://www.effbot.org/zone/metaclass-plugins.htm
+see: 
+
+* https://www.effbot.org/zone/metaclass-plugins.htm
+* https://www.pythoncentral.io/how-metaclasses-work-technically-in-python-2-and-3/
 """
 
 import logging
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 registry = {} # dictionary of subclasses
 
--- tests/trial/spam.py
+++ tests/trial/spam.py
@@ -2,7 +2,10 @@
 """
 test of "Using Metaclasses to Create Self-Registering Plugins"
 
-see: https://www.effbot.org/zone/metaclass-plugins.htm
+see: 
+
+* https://www.effbot.org/zone/metaclass-plugins.htm
+* https://www.pythoncentral.io/how-metaclasses-work-technically-in-python-2-and-3/
 """
 
 from plugin_base import Plugin
