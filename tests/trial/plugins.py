c1b9e7d107b2837a3c7694d730f3a6f5faffd7b9
Pete Jemian <prjemian@gmail.com>
2019-07-12 10:17:39 -0500

    TST #166 import from existing plugins (none will match)

--- tests/trial/plugins.py
+++ tests/trial/plugins.py
@@ -8,6 +8,15 @@ see:
 * https://www.pythoncentral.io/how-metaclasses-work-technically-in-python-2-and-3/
 """
 
+import os
+import sys
+sys.path.append(os.path.join("..", "..", "src"))
+import spec2nexus.plugins.apstools_specwriter_spec2nexus
+import spec2nexus.plugins.fallback_spec2nexus
+import spec2nexus.plugins.spec_common_spec2nexus
+import spec2nexus.plugins.uim_spec2nexus
+import spec2nexus.plugins.unicat_spec2nexus
+import spec2nexus.plugins.XPCS_spec2nexus
 import bacon
 import spam
 
