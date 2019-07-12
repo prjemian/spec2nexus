867582d7575e0c37fcf37f2b2b22b90b775fab14
Pete Jemian <prjemian@gmail.com>
2019-07-12 09:04:45 -0500

    TST #166 prefer logging to print

--- tests/trial/example.py
+++ tests/trial/example.py
@@ -6,12 +6,17 @@ see: https://www.effbot.org/zone/metaclass-plugins.htm
 """
 
 import plugins
+import logging
 
-print("-"*40)
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+logger.info("-"*40)
 
 # loop over registered plugins
 for name, cls in plugins.get_registry():
     if cls is not plugins.plugin_base.Plugin:
-        print(f"name={name}  class={cls}")
+        logger.info(f"name={name}  class={cls}")
 
-print("-"*40)
+logger.info("-"*40)
--- tests/trial/plugin_base.py
+++ tests/trial/plugin_base.py
@@ -5,21 +5,25 @@ test of "Using Metaclasses to Create Self-Registering Plugins"
 see: https://www.effbot.org/zone/metaclass-plugins.htm
 """
 
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 registry = [] # list of subclasses
 
 class Plugin(type):
     def __init__(cls, name, bases, dict):
-        print(" "*4 + "."*10)
-        print(f"__init__: cls={cls}")
-        print(f"__init__: name={name}")
-        print(f"__init__: bases={bases}")
-        print(f"__init__: dict={dict}")
+        logger.debug(" "*4 + "."*10)
+        logger.debug(f"__init__: cls={cls}")
+        logger.debug(f"__init__: name={name}")
+        logger.debug(f"__init__: bases={bases}")
+        logger.debug(f"__init__: dict={dict}")
         registry.append((name, cls))
 
     def __new__(metaname, classname, baseclasses, attrs):
-        print(" "*4 + "."*10)
-        print(f'__new__: metaname={metaname}')
-        print(f'__new__: classname={classname}')
-        print(f'__new__: baseclasses={baseclasses}')
-        print(f'__new__: attrs={attrs}')
+        logger.debug(" "*4 + "."*10)
+        logger.debug(f'__new__: metaname={metaname}')
+        logger.debug(f'__new__: classname={classname}')
+        logger.debug(f'__new__: baseclasses={baseclasses}')
+        logger.debug(f'__new__: attrs={attrs}')
         return type.__new__(metaname, classname, baseclasses, attrs)
