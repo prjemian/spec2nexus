747ee48dd7db9b309700efec32a45bebbef82231
Pete Jemian <prjemian@gmail.com>
2019-07-13 16:40:08 -0500

    MNT #159 just the code to use the writer

--- src/uxml/runner.py
+++ src/uxml/runner.py
@@ -5,31 +5,17 @@ test the UXML plugin (during development)
 
 import spec2nexus.plugin
 import spec2nexus.spec
+import spec2nexus.writer
 
 # load all the supplied plugins BEFORE your custom plugins
 spec2nexus.plugin.load_plugins()
-registry = spec2nexus.plugin.get_registry()
-
-# show our plugin is not loaded
-print("known: ", "#UXML" in registry) # expect False
 
 import uxml_plugin
-# show that our plugin is registered
-print("known: ", "#UXML" in registry) # expect True
-
-# read a SPEC data file, scan 1
-try:
-    spec_data_file = spec2nexus.spec.SpecDataFile("test_1.spec")
-except uxml_plugin.UXML_Error as exc:
-    print(str(exc))
 
 spec_data_file = spec2nexus.spec.SpecDataFile("test_3.spec")
-scan = spec_data_file.getScan(1)
 
-# Do we have our PV data?
-print(hasattr(scan, "UXML"))    # expect False
-
-# must parse full scan before our custom plugin is processed
-scan.interpret()
-print(hasattr(scan, "UXML"))    # expect True
-# print("\n".join(scan.UXML))
+try:
+    out = spec2nexus.writer.Writer(spec_data_file)
+    out.save("data.hdf5", [1])
+except uxml_plugin.UXML_Error as exc:
+    print(str(exc))
