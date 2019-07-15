11c2fe3a49a436fe22b7ad4af0754da172bfb6b2
Pete Jemian <prjemian@gmail.com>
2019-07-15 12:52:10 -0500

    TST #159 wip

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -12,7 +12,10 @@ unit tests for the writer module
 # The full license is in the file LICENSE.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-import os, sys
+import os
+import shutil
+import sys
+import tempfile
 import unittest
 
 _test_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -52,6 +55,36 @@ class TestWriter(unittest.TestCase):
         dd = out.root_attributes()
         self.assertTrue(isinstance(dd, dict))
 
+class TestMeshes(unittest.TestCase):
+
+    def setUp(self):
+        self._owd = os.getcwd()
+        self.tempdir = tempfile.mkdtemp()
+        os.chdir(self.tempdir)
+
+    def tearDown(self):
+        if os.path.exists(self._owd):
+            os.chdir(self._owd)
+        if os.path.exists(self.tempdir):
+            shutil.rmtree(self.tempdir, ignore_errors=True)
+
+    def test_save_data_mesh(self):
+        # #S 22  mesh  eta 57 57.1 10  chi 90.9 91 10  1
+        fname = os.path.join(_path, "spec2nexus", 'data', '33id_spec.dat')
+        hname = "test.h5"
+        spec_data = spec.SpecDataFile(fname)
+        out = writer.Writer(spec_data)
+#         out.save(hname, [22])
+#  
+#         with h5py.File("test.h5", "r") as hp:
+#             root = hp["/"]
+#             nxentry = root["S22"]
+#             self.assertTrue("text" in nxentry)
+#             value = eznx.read_nexus_field(nxentry, "text", astype=str)
+#             self.assertEqual(value, "replacement text")
+
+# --------------
+
 #     sys.argv.append(os.path.join('data', 'APS_spec_data.dat'))
 #     sys.argv.append(os.path.join('data', '33id_spec.dat'))
 #     sys.argv.append(os.path.join('data', '33bm_spec.dat'))
@@ -62,6 +95,7 @@ def suite(*args, **kw):
     test_suite = unittest.TestSuite()
     test_list = [
         TestWriter,
+        TestMeshes,
         ]
     for test_case in test_list:
         test_suite.addTest(unittest.makeSuite(test_case))
