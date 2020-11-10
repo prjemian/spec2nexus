c10201cf8cf218a133dd2537332dbe690ac2a3c4
Pete R. Jemian <prjemian@gmail.com>
2020-11-10 03:00:58 -0600

    QA #223

--- src/spec2nexus/plugins/apstools_specwriter.py
+++ src/spec2nexus/plugins/apstools_specwriter.py
@@ -13,22 +13,22 @@
 
 """
 **#MD** : Bluesky metadata from apstools SpecWriterCallback.
-"""
 
-"""
-#MD APSTOOLS_VERSION = 1.1.0
-#MD BLUESKY_VERSION = 1.5.2
-#MD EPICS_CA_MAX_ARRAY_BYTES = 1280000
-#MD EPICS_HOST_ARCH = linux-x86_64
-#MD OPHYD_VERSION = 1.3.2
-#MD beamline_id = APS USAXS 9-ID-C
-#MD datetime = 2019-04-19 10:04:44.400750
-#MD login_id = usaxs@usaxscontrol.xray.aps.anl.gov
-#MD pid = 27062
-#MD proposal_id = testing Bluesky installation
-#MD purpose = tuner
-#MD tune_md = {'width': -0.004, 'initial_position': 8.824885, 'time_iso8601': '2019-04-19 10:04:44.402643'}
-#MD tune_parameters = {'num': 31, 'width': -0.004, 'initial_position': 8.824885, 'peak_choice': 'com', 'x_axis': 'm_stage_r', 'y_axis': 'I0_USAXS'}
+EXAMPLE::
+
+    #MD APSTOOLS_VERSION = 1.1.0
+    #MD BLUESKY_VERSION = 1.5.2
+    #MD EPICS_CA_MAX_ARRAY_BYTES = 1280000
+    #MD EPICS_HOST_ARCH = linux-x86_64
+    #MD OPHYD_VERSION = 1.3.2
+    #MD beamline_id = APS USAXS 9-ID-C
+    #MD datetime = 2019-04-19 10:04:44.400750
+    #MD login_id = usaxs@usaxscontrol.xray.aps.anl.gov
+    #MD pid = 27062
+    #MD proposal_id = testing Bluesky installation
+    #MD purpose = tuner
+    #MD tune_md = {'width': -0.004, 'initial_position': 8.824885, 'time_iso8601': '2019-04-19 10:04:44.402643'}
+    #MD tune_parameters = {'num': 31, 'width': -0.004, 'initial_position': 8.824885, 'peak_choice': 'com', 'x_axis': 'm_stage_r', 'y_axis': 'I0_USAXS'}
 """
 
 from collections import OrderedDict
