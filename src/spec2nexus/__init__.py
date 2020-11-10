75f1afa23f32c31e70d02b7d58ab07640909e0bb
Pete R. Jemian <prjemian@gmail.com>
2020-11-10 02:11:33 -0600

    TST #221

--- src/spec2nexus/__init__.py
+++ src/spec2nexus/__init__.py
@@ -42,6 +42,7 @@ __classifiers__ = [
      'Programming Language :: Python :: 3.5',
      'Programming Language :: Python :: 3.6',
      'Programming Language :: Python :: 3.7',
+     'Programming Language :: Python :: 3.8',
      'Topic :: Scientific/Engineering',
      'Topic :: Scientific/Engineering :: Astronomy',
      'Topic :: Scientific/Engineering :: Bio-Informatics',
