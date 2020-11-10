b5f67ba47e6b0c15a521bb132accf6b1c4e267c1
Pete R. Jemian <prjemian@gmail.com>
2020-11-10 02:57:43 -0600

    PKG #223

--- publish.sh
+++ publish.sh
@@ -4,21 +4,31 @@
 
 ## Define the release
 
-PACKAGE=`python setup.py --name`
-RELEASE=`python setup.py --version`
-echo "PACKAGE: ${PACKAGE}"
-echo "RELEASE: ${RELEASE}"
+STARTED=$(date)
+PACKAGE=$(python setup.py --name)
+RELEASE=$(python setup.py --version)
+echo "# ($(date)) PACKAGE: ${PACKAGE}"
+echo "# ($(date)) RELEASE: ${RELEASE}"
 
 if [[ ${RELEASE} == *dirty || ${RELEASE} == *+* ]] ; then
-  echo "version: ${RELEASE} not ready to publish"
+  echo "# version: ${RELEASE} not ready to publish"
   exit 1
 fi
 
+echo "# ($(date)) - - - - - - - - - - - - - - - - - - - - - - build for PyPI"
+
 ## PyPI Build and upload::
 
-echo "Building for upload to PyPI"
+echo "# ($(date)) Building for upload to PyPI"
 python setup.py sdist bdist_wheel
-twine upload dist/${PACKAGE}-${RELEASE}*
+echo "# ($(date)) Built for PyPI"
+
+echo "# ($(date)) - - - - - - - - - - - - - - - - - - - - - - upload to PyPI"
+
+twine upload "dist/${PACKAGE}-${RELEASE}*"
+echo "# ($(date)) Uploaded to PyPI"
+
+echo "# ($(date)) - - - - - - - - - - - - - - - - - - - - - - conda build"
 
 ## Conda Build and upload::
 
@@ -31,21 +41,30 @@ else
   # production releases
   CHANNEL=aps-anl-tag
 fi
+echo "# ($(date)) CHANNEL: ${CHANNEL}"
 
 ### publish (from linux)
 
-echo "Building for upload to conda"
+echo "# ($(date)) Building for upload to conda"
 
 export CONDA_BLD_PATH=/tmp/conda-bld
 /bin/mkdir -p ${CONDA_BLD_PATH}
 
-export LOG_FILE=${CONDA_BLD_PATH}/${PACKAGE}-${RELEASE}-conda-build.log
-conda build ./conda-recipe/ 2>&1 | tee ${LOG_FILE}
+conda build ./conda-recipe/
+echo "# ($(date)) Built for conda"
 
-echo "upload to conda CHANNEL: ${CHANNEL}"
-_package_=$(echo ${PACKAGE} | tr '[:upper:]' '[:lower:]')
-BUNDLE=${CONDA_BLD_PATH}/noarch/${_package_}-${RELEASE}-*_0.tar.bz2
-anaconda upload -u ${CHANNEL} ${BUNDLE}
+echo "# ($(date)) - - - - - - - - - - - - - - - - - - - - - - upload conda"
+
+BUILD_DIR=${CONDA_BLD_PATH}/noarch
+_package_=$(echo "${PACKAGE}" | tr '[:upper:]' '[:lower:]')
+BUNDLE="${BUILD_DIR}/${_package_}-${RELEASE}-py_0.tar.bz2"
+echo "# ($(date)) uploading to anaconda"
+echo "# ($(date)) CHANNEL: ${CHANNEL}"
+anaconda upload -u "${CHANNEL}" "${BUNDLE}"
 
 # also post to my personal channel
-anaconda upload ${BUNDLE}
+anaconda upload "${BUNDLE}"
+echo "# ($(date)) Uploaded to anaconda"
+
+echo "# (${STARTED}) started publishing script"
+echo "# ($(date)) finished publishing script"
