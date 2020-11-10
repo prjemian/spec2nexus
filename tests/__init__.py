5497a9266615719ce77151959034a5e95afaf992
Pete R. Jemian <prjemian@gmail.com>
2020-11-10 11:16:04 -0600

    TST #223 sort

--- tests/__init__.py
+++ tests/__init__.py
@@ -24,9 +24,19 @@ if _path not in sys.path:
 
 def suite(*args, **kw):
     from tests import test_data_03_06_JanTest
-    from tests import test_extractSpecScan
     from tests import test_diffractometers
+    from tests import test_extractSpecScan
     from tests import test_eznx
+    from tests import test_issue107
+    from tests import test_issue119
+    from tests import test_issue123
+    from tests import test_issue161
+    from tests import test_issue188
+    from tests import test_issue191
+    from tests import test_issue216
+    from tests import test_issue64
+    from tests import test_issue8
+    from tests import test_issue99_hklscan
     from tests import test_md_apstools_specwriter
     from tests import test_multiple_headers
     from tests import test_nexus
@@ -39,37 +49,12 @@ def suite(*args, **kw):
     from tests import test_uxml
     from tests import test_writer
     from tests import test_XPCS
-    from tests import test_issue8
-    from tests import test_issue64
-    from tests import test_issue99_hklscan
-    from tests import test_issue107
-    from tests import test_issue119
-    from tests import test_issue123
-    from tests import test_issue161
-    from tests import test_issue188
-    from tests import test_issue191
-    from tests import test_issue216
 
     test_list = [
         test_data_03_06_JanTest,
         test_diffractometers,
         test_extractSpecScan,
         test_eznx,
-        test_md_apstools_specwriter,
-        test_multiple_headers,
-        test_nexus,
-        test_plugin,
-        test_scanf,
-        test_spec,
-        test_specplot,
-        test_specplot_gallery,
-        test_utils,
-        test_uxml,
-        test_writer,
-        test_XPCS,
-        test_issue8,
-        test_issue64,
-        test_issue99_hklscan,
         test_issue107,
         test_issue119,
         test_issue123,
@@ -77,6 +62,21 @@ def suite(*args, **kw):
         test_issue188,
         test_issue191,
         test_issue216,
+        test_issue64,
+        test_issue8,
+        test_issue99_hklscan,
+        test_md_apstools_specwriter,
+        test_multiple_headers,
+        test_nexus,
+        test_plugin,
+        test_scanf,
+        test_spec,
+        test_specplot_gallery,
+        test_specplot,
+        test_utils,
+        test_uxml,
+        test_writer,
+        test_XPCS,
     ]
 
     test_suite = unittest.TestSuite()
