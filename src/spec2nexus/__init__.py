01f4e148dd3ef10cd659827bfe295d77d20fcfb8
Pete R Jemian <prjemian@gmail.com>
2021-11-08 18:50:55 -0600

    CI #234 Py3.7-9

--- src/spec2nexus/__init__.py
+++ src/spec2nexus/__init__.py
@@ -52,7 +52,7 @@ __classifiers__ = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3.10",
+    # "Programming Language :: Python :: 3.10",
     "Topic :: Scientific/Engineering",
     "Topic :: Scientific/Engineering :: Astronomy",
     "Topic :: Scientific/Engineering :: Bio-Informatics",
