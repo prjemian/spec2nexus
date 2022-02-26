939542750f9f382d88352df639c07a709a279809
Pete R Jemian <prjemian@gmail.com>
2022-02-26 13:45:34 -0600

    MNT #246 apply black code style to demos

--- demo/usaxs_uascan.py
+++ demo/usaxs_uascan.py
@@ -1,58 +1,62 @@
 #!/usr/bin/env python
 
-'''
+"""
 Plot data from the USAXS uascan macro
 
 .. autosummary::
 
     ~UAscan_Plotter
 
-'''
+"""
 
 import spec2nexus.specplot
 import spec2nexus.specplot_gallery
 
 
 class UAscan_Plotter(spec2nexus.specplot.LinePlotter):
-    '''simple customize of `uascan` handling'''
+    """Customized `uascan` handling"""
 
     def retrieve_plot_data(self):
-        '''plot the vertical axis on log scale'''
+        """plot the vertical axis on log scale"""
         spec2nexus.specplot.LinePlotter.retrieve_plot_data(self)
 
         if self.signal in self.data:
             if min(self.data[self.signal]) <= 0:
                 # TODO: remove any data where Y <= 0 (can't plot on log scale)
-                msg = 'cannot plot Y<0: ' + str(self.scan)
+                msg = "cannot plot Y<0: " + str(self.scan)
                 raise spec2nexus.specplot.NotPlottable(msg)
 
         # in the uascan, a name for the sample is given in `self.scan.comments[0]`
         self.set_y_log(True)
         self.set_plot_subtitle(
-            '#%s uascan: %s' % (str(self.scan.scanNum), self.scan.comments[0]))
+            "#%s uascan: %s" % (str(self.scan.scanNum), self.scan.comments[0])
+        )
 
 
 def debugging_setup():
     import os, sys
     import shutil
     import ascan
+
     selector = spec2nexus.specplot.Selector()
-    selector.add('ascan', ascan.Custom_Ascan)   # just for the demo
-    path = '__usaxs__'
+    selector.add("ascan", ascan.Custom_Ascan)  # just for the demo
+    path = "__usaxs__"
     shutil.rmtree(path, ignore_errors=True)
     os.mkdir(path)
-    sys.argv.append('-d')
+    sys.argv.append("-d")
     sys.argv.append(path)
-    sys.argv.append(os.path.join('..', 'src', 'spec2nexus', 'data', 'APS_spec_data.dat'))
+    sys.argv.append(
+        os.path.join("..", "src", "spec2nexus", "data", "APS_spec_data.dat")
+    )
 
 
 def main():
     selector = spec2nexus.specplot.Selector()
-    selector.add('uascan', UAscan_Plotter)
+    selector.add("uascan", UAscan_Plotter)
     spec2nexus.specplot_gallery.main()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # debugging_setup()
     main()
 
