d26d16365b91883156128b6e67653ec6354fa113
Pete R Jemian <prjemian@gmail.com>
2022-12-22 09:46:19 -0600

    MNT #298

--- spec2nexus/__init__.py
+++ spec2nexus/__init__.py
@@ -5,7 +5,7 @@
 try:
     from setuptools_scm import get_version
 
-    __version__ = get_version()
+    __version__ = get_version(root="..", relative_to=__file__)
     del get_version
 except (LookupError, ModuleNotFoundError):
     from importlib.metadata import version
